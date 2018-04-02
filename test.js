8b14d3b11d0accbc72f206b2ecf7eb62ea782035
zhanglei25 <zhanglei25@le.com>
2018-04-02 18:30:14 +0800

    2018.4.2

--- test.js
+++ test.js
@@ -9,4 +9,87 @@
      * 2018.4.2
      */
 
+    /* 1.写一个函数模拟new操作符的功能 */
+    function newFunc(Parent) {
+        var res = {};
+        res.__proto__ = Parent.prototype; //res成为构造函数Parent的实例
+        res.call(Parent); // 执行构造函数
+        return res; //返回匿名新对象
+    }
+
+    /* 2.数组去重 */
+    function unique(arr) { // 复杂度O(n^2)
+        arr.sort();
+        var res = [arr[0], ];
+
+        for (var i = 1, len = arr.length; i < len; i++) {
+            if (arr[i] == arr[i - 1]) {
+                continue;
+            } else {
+                res.push(arr[i]);
+            }
+        }
+        return res;
+    }
+
+    function unique(arr) { // 复杂度O(n^2)
+        var res = [];
+        for (var i = 0, l = arr.length; i < l; i++) {
+            if (!res.includes(arr[i])) {
+                res.push(arr[i]);
+            }
+        }
+        return res;
+    }
+
+    function unique(arr) { // ES6 Set数据结构，成员值唯一，是一种新的类数组对象
+        return Array.from(new Set(arr));
+        // 或者
+        // return [...new Set(arr)];
+    }
+
+    /* 3. js对象深拷贝 */
+    function deepCopy(obj) {
+        var res, objType;
+        var type = typeof obj;
+
+        switch (type) {
+            case 'number':
+            case 'boolean':
+            case 'undefined':
+            case 'string':
+                res = obj;
+                break;
+            case 'object':
+                objType = Object.prototype.toString.call(obj).slice(8, -1);
+                switch (objType) {
+                    case 'Function':
+                        res = obj;
+                        break;
+                    case 'Array':
+                        var resArr = [];
+                        obj.forEach((e) => {
+                            resArr.push(deepCopy(e));
+                        })
+                        res = resArr;
+                        break;
+                    case 'Object':
+                        var resObj = {};
+                        for (var key in obj) {
+                            resObj[key] = deepCopy(obj.key);
+                        }
+                        res = resObj;
+                        break;
+                    case 'Null':
+                        res = null;
+                        break;
+                    default:
+                        break;
+                }
+            default:
+                break;
+        }
+        return res;
+    }
+
 }())
